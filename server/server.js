fc44b036504ce235182ee9f3ecf7971fff858087
Vic Soto <itsvicsoto@gmail.com>
2014-12-17 14:19:26 +0800

    updates

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    config = require('./config'),
+    app = express();
 
-app.get('/', function(req, res){
+app.get('/404', function(req, res){
   res.send('hello world');
 });
 
 // Express Server Config
 app.set('port', process.env.PORT || 5005);
+
+// Default Application Directories that are served.
+app.use('/app', express.static(__dirname + config.root + '/app'));
+app.use('/assets', express.static(__dirname + config.root + '/assets'));
+
+// Redirect all requests to the application root's index.html
+// so AngularJS could handle routes unless specified in this file
+app.get('*', function (req, res) {
+  res.sendFile('index.html', { root: __dirname + config.root });
+});
 
 app.listen(app.get('port'), function () {
   var meta = module.exports = require('./../package.json');
